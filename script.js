06e2e94e96154874cdffb0b7c11d29ee23998a27
Michell <72279305+mgoris18@users.noreply.github.com>
2023-03-23 17:31:56 -0400

    dark mode

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
 const todos = document.getElementById('todos');
+const toggle = document.getElementById('toggle');
+const body = document.querySelector('body');
+const header = document.querySelector('h1');
+const small = document.querySelector('small')
+
+toggle.addEventListener("click", () => toggle.classList.toggle("active")); 
+toggle.addEventListener("click",() => body.classList.toggle("active"));
+toggle.addEventListener("click",() => header.classList.toggle("active"));
+toggle.addEventListener("click",() => form.classList.toggle("active"));
+toggle.addEventListener("click",() => input.classList.toggle("active"));
+toggle.addEventListener("click",() => todos.classList.toggle("active"));
+toggle.addEventListener("click",() => small.classList.toggle("active"));
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
